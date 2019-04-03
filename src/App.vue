9ac03e869020ec3554c663f7221f49c89c8d5678
Daniel Schmitz <danieljfa@gmail.com>
2019-04-03 14:49:21 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-form>
+      <v-container>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-text-field ref='text' v-model="text" label="Texto Completo" append-icon='content_copy'
+    @click:append='copy("text")'></v-text-field>
+          </v-flex>
+
+          <v-flex xs12>
+            <v-text-field ref='key' v-model="keytext" label="Key text" append-icon='content_copy'
+    @click:append='copy("key")'></v-text-field>
+          </v-flex>
+
+          <v-flex xs12>
+            <v-text-field ref='complete' v-model="complete" label="Complete Text" append-icon='content_copy'
+    @click:append='copy("complete")'></v-text-field>
+          </v-flex>
+
+        </v-layout>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    copy(inputField) {
+      const input = this.$refs[inputField];
+      input.focus();
+      document.execCommand('selectAll');
+      this.copied = document.execCommand('copy');
+    },
+  },
+  computed: {
+    keytext () {
+        return this.text.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/ /g,"_").toLowerCase();
+    },
+    complete () {
+      return `${this.keytext}: '${this.text}',`
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
