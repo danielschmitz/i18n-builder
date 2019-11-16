fea9a64ca27569dc630d4a9b85d5fc012e9aabdf
Daniel Schmitz <danieljfa@gmail.com>
2019-11-16 11:57:13 -0300

    melhorias

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,20 @@
     <v-form>
       <v-container>
         <v-layout row wrap>
+          <v-flex xs3>
           <v-btn @click="paste()">paste</v-btn>
+          </v-flex>
+
+           <v-flex xs9>
+            <v-text-field
+              ref="complete"
+              v-model="complete"
+              label="key:value"
+              append-icon="content_copy"
+              @click:append="copy('complete')"
+            ></v-text-field>
+
+          </v-flex>
 
           <v-flex xs12>
             <v-text-field
@@ -15,7 +28,7 @@
             ></v-text-field>
           </v-flex>
 
-          <v-flex xs12>
+          <v-flex xs6>
             <v-text-field
               ref="key"
               v-model="keytext"
@@ -25,11 +38,11 @@
             ></v-text-field>
           </v-flex>
 
-          <v-flex xs12>
+          <v-flex xs6>
             <v-text-field
               ref="tkey"
               v-model="tkey"
-              label="tKey text"
+              label="Template"
               append-icon="content_copy"
               @click:append="copy('tkey')"
             ></v-text-field>
@@ -37,21 +50,21 @@
 
           
 
-          <v-flex xs12>
+          <v-flex xs6>
             <v-text-field
               ref="tkey2"
               v-model="tkey2"
-              label="tKey text"
+              label="Script"
               append-icon="content_copy"
               @click:append="copy('tkey2')"
             ></v-text-field>
           </v-flex>
 
-            <v-flex xs12>
+          <v-flex xs6>
             <v-text-field
               ref="tkey3"
               v-model="tkey3"
-              label="tKey text"
+              label="Script"
               append-icon="content_copy"
               @click:append="copy('tkey3')"
             ></v-text-field>
@@ -67,16 +80,49 @@
             ></v-text-field>
           </v-flex>
 
+          <v-flex xs6>
+            <v-text-field
+              ref="tkey5"
+              v-model="tkey5"
+              label="floatLabel"
+              append-icon="content_copy"
+              @click:append="copy('tkey5')"
+            ></v-text-field>
+          </v-flex>
+
+          <v-flex xs6>
+            <v-text-field
+              ref="tkey6"
+              v-model="tkey6"
+              label="floatLabel *"
+              append-icon="content_copy"
+              @click:append="copy('tkey6')"
+            ></v-text-field>
+          </v-flex>
+
+            <v-flex xs6>
+            <v-text-field
+              ref="tkey7"
+              v-model="tkey7"
+              label="data-vv"
+              append-icon="content_copy"
+              @click:append="copy('tkey7')"
+            ></v-text-field>
+          </v-flex>
           
 
-          <v-flex xs12>
+             <v-flex xs6>
             <v-text-field
-              ref="complete"
-              v-model="complete"
-              label="Complete Text"
+              ref="tkey8"
+              v-model="tkey8"
+              label="i18n"
               append-icon="content_copy"
-              @click:append="copy('complete')"
+              @click:append="copy('tkey8')"
             ></v-text-field>
+          </v-flex>
+
+        
+
           </v-flex>
         </v-layout>
       </v-container>
@@ -118,20 +164,36 @@ export default {
         .replace(/[\u0300-\u036f]/g, "")
         .replace(/ /g, "_")
         .replace(/'/g, "")
+        .replace(/,/g, "")
+        .replace(/\./g, "")
+        .replace(/:/g, "")
+        .replace(/\\/g, "")
         .replace(/-/g, "_")
         .toLowerCase();
     },
-    tkey () { //{{ $t('bem_vindo') }}
+    tkey () {
       return `{{ $t('${this.keytext}') }}`
     },
-    tkey2 () { //{{ $t('bem_vindo') }}
+    tkey2 () {
       return `this.$t('${this.keytext}')`
     },
-     tkey3 () { //{{ $t('bem_vindo') }}
+     tkey3 () {
       return `$t('${this.keytext}')`
     },
-     tkey4 () { //{{ $t('bem_vindo') }}
+     tkey4 () {
       return `default: function () { return this.$t('${this.keytext}') }`
+    },
+     tkey5 () {
+      return `:floatLabel="$t('${this.keytext}')"`
+    },
+      tkey6 () {
+      return `:floatLabel="$t('${this.keytext}') + ' *'"`
+    },
+      tkey7 () {
+      return `data-vv-as="$t('${this.keytext}')"`
+    },
+     tkey8 () {
+      return `i18n.t('${this.keytext}')`
     },
     complete () {
       return `${this.keytext}: '${this.text.replace(/'/g, "")}',`
@@ -146,6 +208,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
